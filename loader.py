80b860f12095f20ba75110803055b29b7de402c8
FSharp4 <jakepeterson163@gmail.com>
2022-02-09 04:21:14 -0500

    Update loader.py
    - Cluster information is saved

--- loader.py
+++ loader.py
@@ -7,9 +7,13 @@ def write(filename, data: Dataset):
     with open(filename, "w", newline='') as file:
         writer = csv.writer(file)
         headings = feature_headings(data.features[0].__len__())
-        rows = [ headings ]
-        for feature in data.features:
-            rows.append(feature)
+        headings.append("Cluster Index")
+        rows = [headings]
+        for i in range(0, data.features.__len__()):
+            feature = data.features[i]
+            cluster = data.clusters[i]
+            row = feature.tolist()
+            row.append(cluster)
+            rows.append(row)
 
         writer.writerows(rows)
-
