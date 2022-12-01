dc0b7d92d78f5f25afb024f272a4878756148cae
FSharp4 <33583307+FSharp4@users.noreply.github.com>
2022-12-01 04:25:51 -0600

    Figure for one of the final report diagrams

--- _dev/smallest.py
+++ _dev/smallest.py
@@ -6,4 +6,11 @@ from data.generatedata import generate_gan
 
 if __name__ == "__main__":
     smallest = generate_gan().features
+    Z = hierarchy.linkage(smallest, method="ward")
+    plt.figure()
+    hierarchy.dendrogram(Z)
+    plt.xlabel("Datapoint Index")
+    plt.ylabel("(Dis)similarity Metric")
+    plt.title("Example Dendrogram")
+    plt.show()
 
